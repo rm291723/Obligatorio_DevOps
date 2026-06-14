394bd63a878e37b0bdecbf9c700d7191237d5656
Ricardo Menendez <root@localhost.localdomain>
2026-06-14 19:25:53 -0300

    fix: revert ECR to MUTABLE tags required for latest tag pipeline

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -2,7 +2,7 @@ resource "aws_ecr_repository" "this" {
   for_each = toset(var.services)
 
   name                 = "${var.project_name}/${each.value}"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
