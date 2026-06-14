09b79f3dae5980ebfbe239e312a26cd683c33c47
Ricardo Menendez <root@localhost.localdomain>
2026-06-14 18:47:48 -0300

    fix: use nosemgrep to suppress public subnet finding

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = true # nosemgrep: aws-subnet-has-public-ip-address
+  map_public_ip_on_launch = true # nosemgrep
 
   tags = {
     Name        = "${var.project_name}-public-subnet-${count.index + 1}-${var.environment}"
