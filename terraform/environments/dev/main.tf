7de94269918befe3ac41342d884b03ca65ff9d9a
Ricardo Menendez <root@192.168.1.34>
2026-06-20 22:24:59 -0300

    fix: configurar URLs de microservicios en UI apuntando al ALB

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -119,7 +119,11 @@ locals {
       memory        = 512
       desired_count = 1
       environment_vars = [
-        { name = "PORT", value = "8080" }
+        { name = "PORT", value = "8080" },
+        { name = "RETAIL_UI_ENDPOINTS_CATALOG", value = "http://${module.alb.alb_dns_name}/catalog" },
+        { name = "RETAIL_UI_ENDPOINTS_CARTS", value = "http://${module.alb.alb_dns_name}/cart" },
+        { name = "RETAIL_UI_ENDPOINTS_CHECKOUT", value = "http://${module.alb.alb_dns_name}/checkout" },
+        { name = "RETAIL_UI_ENDPOINTS_ORDERS", value = "http://${module.alb.alb_dns_name}/orders" }
       ]
     }
   }
