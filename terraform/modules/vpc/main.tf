7be2105294ea6b4dc3257579efe99decd68e23f0
Ricardo Menendez <root@localhost.localdomain>
2026-06-30 19:00:04 -0300

    fix: usar variables en count de route_table_association para evitar error en plan

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -42,7 +42,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
+  count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
@@ -86,7 +86,7 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private" {
-  count          = length(aws_subnet.private)
+  count          = length(var.private_subnet_cidrs)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 }
